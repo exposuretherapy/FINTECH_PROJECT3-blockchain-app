6129aa533923cea5dcd70b5a59ac17ea690dc18f
Patton720 <Jcpatton720@gmail.com>
2020-05-21 21:30:13 -0500

    adding more code 2 functions

--- MedicalHistory.sol
+++ MedicalHistory.sol
@@ -12,27 +12,53 @@ contract MedicalHistory {
     Counters.Counter Hospital_ids;
     Counters.Counter Procedure_ids;
     
-    event Procedure (uint patient_id, uint doctor_id, uint hospital_id, uint procedure_ids, string uri );
-    event NewDoctor (uint patient_id, uint doctor_id, uint hospital_id, unit procedure_ids, string uri);
-    event Death (uint patient_id, uint doctor_id, uint hospital_id, uint procedure_ids, string uri);
+    event Procedure (uint patient_id, uint doctor_id, uint hospital_id, uint procedure_id, string uri );
+    event NewProcedure (uint Procedure_id, string name, string uri);
+    event NewDoctor (uint doctor_id, string uri);
+    event Death (uint patient_id, uint doctor_id, uint hospital_id, uint procedure_id, string uri);
     event NewHospital(uint hospital_id, string uri);
     event NewPatient(uint patient_id, string uri);
     // events for doctor_id, patient_id, hospital_id 
+    
+    //mapping
+    mapping (uint => string ) Doctors;
+    mapping (uint => string) Hospitals;
+    mapping (uint => sting) Patients;
+    //access levels 1-4
+    mapping(doctor_id => uint) accessLevels;
     
     constructor(
         address payable "_Admin") private {
             Admin = _Admin;
         }
         
+        
+        
+        function createProcedure( string memory uri) public {
+            uint procedure_id,  string name 
+        }
+        
         function createDoctor( string memory uri) public { 
-            generate id, name, sex, licens, specialty,status
+           // generate id, name, sex, licens, specialty,status
+            doctor_ids.increment();
+            doctor_id = doctor_ids.current();
+            _setUri(doctorid.current, string memory uri);
+            
+            emit NewDoctor( uint doctor_id, string uri);
+            
         }
         function updateDoctor( uint doctor_id,string memory uri) public {
              name, sex, licens, specialty, status
         }
         
         function createHospital ( string memory uri) public{
-            generate Id, name, address,status
+            //generate Id, name, address,status
+            hospital_id.increment();
+            hospital_id = hospital_id.current();
+            _setUri( hospital_id, uri);
+            
+            emit NewHospital( hospital_id, uri);
+            
         }
         
         function updateHospital( uint hospital_id, string memory uri) public {
@@ -40,7 +66,12 @@ contract MedicalHistory {
         }
         
         function createPatient ( string memory uri) public {
-            generate id, name, DOB, Blood type, Insurance, sex, status
+            //generate id, name, DOB, Blood type, Insurance, sex, status
+            patient_id.increment();
+            patient_id = patient_id.current();
+            _setUri ( patient_id, uri);
+            
+            emit NewPatient ( patient_id, uri);
         }
         
         function doctorVisit (uint patient_id, uint doctor_id, uint hospital_id,  uint procedure_ids, string memory uri) public {
@@ -56,19 +87,22 @@ contract MedicalHistory {
         }
         
         function createERdata ( uint patient_id, string memory uri) public {
-            allergies, HRF, prescriptions, height, weight
+            //allergies, HRF, prescriptions, height, weight, emergency contact
+            
         }
         
         function UpdateErdata (uint patient_id, string memory uri) public {
             allergies, HRF, prescriptions, height, weight
         }
         
-        function registerProcedures( string memory uri) public {
-            generate procedure id, name
-        }
+        function CreateProcedures( string memory uri) public {
+            //generate procedure id, name
+            procedure_id.increment();
+            procedure_id = procedure_id.current();
+            _setUri(procedure_id , uri)
             
-        //emits are within a function
-        emit 
-        emit CheckUp
-        emit Death
+            emit NewProcedure( uint procedure_id, string uri);
+            }
+            
+      
 }
